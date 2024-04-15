09578489ac65972f98122935dfff8ca14f6df10c
Maliphos <turbulnceofawsm@gmail.com>
2024-04-15 16:20:51 -0500

    New functions for inclusion of Arduino, still needs testing

--- src/main.c
+++ src/main.c
@@ -66,42 +66,25 @@
 #define dig_P8_REG 0x9C
 #define dig_P9_REG 0x9E
 
-uint8_t resetbme = 0xB6;
-
 //Declarations
 signed long int t_fine; // global variable 
 
 float temperature, humidity, pressure; //variables for each data. 
-float temperatures[240];//array for all temps stored in an hour
+float temperatures[4];//array for room temps used in damper adjustment
+float humidities[4];//array for room humidities
+float pressures[4];//array for room pressures
+float tempavgs[240];//temperatures for a whole hour to avg together and store
+float humidavgs[240];//humidities for a whole hour to avg together and store
+float pressureavgs[240];//pressures for a whole hour to avg together and store
 
-uint8_t deviceID;
+// get temperature for each sensor from arduino
+void readTemp(); 
 
-//Forces a sample of the BME280. Also sets oversampling for humidity, temp and press = 1.
-//Consult the BME280 Datasheet to change these options. 
-void BME280_init();
+// gets humidity for each sensor from arduino
+void readHumidity();
 
-//Write a byte to a register via SPI 
-void writeSPI(char, char);
-
-// return a unsigned 16-bit value 
-unsigned int readSPI16bit(char);
-
-// return a unsigned 8-bit value
-unsigned char readSPI8bit(char);
-// returns a unsigned 16-bit (little endian) 
-unsigned int readSPI16bit_u_LE(char);
-
-// returns a unsigned 16-bit (little endian)
-signed int readSPI16bit_s_LE(char); 
-
-// get temperature and returns it in Celsius
-float readTemp(); 
-
-// gets RH humidity and returns it as a percentage
-float readHumidity();
-
-// gets pressure and returns it in kPa.
-float readPressure(); 
+// gets pressure for each sensor from arduino
+void readPressure(); 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Main Entry Point
@@ -110,52 +93,14 @@ float readPressure();
 
 int main ( void )
 { 
-    char *formattedTemp;
     /* Initialize all modules */
     SYS_Initialize ( NULL );
-    deviceID = 0x00;
-    
-    CS0_Set();
-    
-    CS0_Clear();
-    BME280_init(); // starts a new sample 
-    CS0_Set();
-//    while(true){
-//        Damper1_Clear();
-//    }
     
     while ( true ){
-        CS0_Clear();
-        DelaySec(2);
-        writeSPI(reset_REG, resetbme); // reset the BME280
-        CS0_Set();
-        
-        CS0_Clear();
-        DelaySec(2);
-        BME280_init(); // starts a new sample 
-        CS0_Set();
-        
-        deviceID = 0x00;
-        CS0_Clear();
-        deviceID = readSPI8bit(BME280_REG_CHIP_ID); // reads device ID
-        CS0_Set();
-        printf("ID = %X\n",deviceID);
-        
-        CS0_Clear();
-        DelaySec(2);
-        temperature = readTemp(); // reads temp sample 
-        CS0_Set();
-        printf("Celsius Temp: %f\n",temperature);
-        
-        
-        temperature = temperature*9/5 + 32; // convert to Fahrenheit
-          
-        printf("F temp: %f\n",temperature);
-        
-        CS0_Clear();
-        humidity = readHumidity(); // reads temp sample 
-        CS0_Set();
-        DelaySec(10);
+        readTemp();
+        readHumidity();
+        readPressure();
+        DelaySec(5);
     }
 
     /* Execution should not come here during normal operation */
@@ -163,132 +108,26 @@ int main ( void )
     return ( EXIT_FAILURE );
 }
 
-unsigned char readSPI8bit(char reg){
-    uint8_t regData;
-    SERCOM3_SPI_Write(&reg,1);
-    while(SERCOM3_SPI_IsBusy()){
-        WDT_Clear();
-    } 
-    SERCOM3_SPI_Read(&regData,1);
-    while(SERCOM3_SPI_IsBusy()){
-        WDT_Clear();
+//read temperature of sensors
+void readTemp(void){
+    //send command to get temperatures
+    SERCOM_USART_Write("0",1);
+    while(SERCOM_USART_IsBusy()){
     }
-    return regData; 
-}
-
-unsigned int readSPI16bit(char reg){
-    unsigned int val;
-    val = readSPI8bit(reg); // shift in MSB
-    val = val << 8 | readSPI8bit(reg+1); // shift in LSB
-    return val;
-}
-
-signed int readSPI16bit_s(char reg){
-return (signed int) readSPI16bit(reg);
-}
-
-unsigned int readSPI16bit_u_LE(char reg){ // read 16-bits unsigned little endian
-    unsigned int val;
-    val = readSPI16bit(reg); 
-    return (val >> 8) | (val << 8); // swap upper and lower regs
-}
-
-signed int readSPI16bit_s_LE(char reg) { // read 16-bit signed little endian
-    return (signed int) readSPI16bit_u_LE(reg);
-}
-
-void writeSPI(char reg, char data){
-    reg = reg & 0x7F; //mask off first bit to specify a write. 
+    SERCOM_USART_Read(temperatures,4);
     
-    SERCOM3_SPI_Write(&reg,1);
-    while(SERCOM3_SPI_IsBusy()){
-        WDT_Clear();
-    } 
-    SERCOM3_SPI_Write(&data,1);
-    while(SERCOM3_SPI_IsBusy()){
-        WDT_Clear();
-    } 
+    return ;
+}
+
+//read humidity of sensors
+void readHumidity(void){
     return;
 }
 
-void setReg(char reg, char data){
-    reg = reg & 0x7F;
-    uint8_t txregs[] = {reg, data};
-    SERCOM3_SPI_Write(&txregs,2);
-    while(SERCOM3_SPI_IsBusy()){
-        WDT_Clear();
-    } 
+
+//read pressure of sensors
+void readPressure(){
     return;
-}
-
-void BME280_init(){
-    uint8_t setuparr[4] = {0x00, 0x01, 0x25};
-    setReg(BME280_REG_CTRL_MEAS, setuparr[0]);
-    setReg(BME280_REG_CTRL_HUMIDITY, setuparr[1]); // 1Forced, 3normal mode, Humidity oversampling = 1
-    setReg(BME280_REG_CONFIG, setuparr[0]);//write to config register
-    setReg(BME280_REG_CTRL_MEAS, setuparr[2]); // 25,B7Forced mode,27normal mode, Temp/Press oversampling = 1
-}
-
-float readTemp(void){
-    // Calibration Coefficients:
-    unsigned long int dig_T1 = readSPI16bit_u_LE(dig_T1_REG); 
-    signed long int dig_T2 = readSPI16bit_s_LE(dig_T2_REG);
-    signed long int dig_T3 = readSPI16bit_s_LE(dig_T3_REG);
-    
-    CS0_Set();
-    DelaySec(2);
-    CS0_Clear();
-    DelaySec(2);
-    
-    // Temperature Raw ADC:
-    unsigned long int adc_T = 0;
-    adc_T = readSPI16bit_u_LE(TEMP_REG);
-    adc_T <<= 8; // move in XLSB register
-    adc_T |= readSPI8bit(TEMP_REG + 2);
-    adc_T >>= 4; // Only uses top 4 bits of XLSB register 
-
-    // From BME280 data sheet: 
-    signed long int var1  = ((((adc_T>>3) - (dig_T1 <<1))) *
-	   (dig_T2)) >> 11;
-  
-    signed long int var2  = (((((adc_T>>4) - (dig_T1)) *
-	     ((adc_T>>4) - (dig_T1))) >> 12) *
-	     (dig_T3)) >> 14;
-
-    t_fine = var1 + var2;
- 
-    float T = (t_fine * 5 + 128) >> 8;
-    return T/100;
-}
-
-float readHumidity(void){
-    // Calibration Coefficients
-    unsigned int dig_H1 = readSPI8bit(dig_H1_REG);
-    signed long int dig_H2 = readSPI16bit_s_LE(dig_H2_REG);
-    unsigned int dig_H3 = readSPI8bit(dig_H3_REG);
-    signed long int dig_H4 = (readSPI8bit(dig_H4_REG)<<4)|(readSPI8bit(dig_H4_REG+1) & 0xF);
-    signed long int dig_H5 = (readSPI8bit(dig_H5_REG+1)<<4)|(readSPI8bit(dig_H5_REG)>>4);
-    signed int dig_H6 = (signed int) readSPI8bit(dig_H6_REG);
-    
-    //Humidity raw ADC
-    unsigned long int adc_H = readSPI16bit(HUM_REG);
-    
-    //compensate
-    unsigned long int v_x1_u32r;
-    v_x1_u32r = t_fine - 76800;
-    
-    v_x1_u32r = (((((adc_H << 14) - ((dig_H4) << 20) - ((dig_H5) * v_x1_u32r))
-            + (16384)) >> 15) * (((((((v_x1_u32r * (dig_H6)) >> 10) *
-		    (((v_x1_u32r * (dig_H3)) >> 11) + (32768))) >> 10) +
-            (2097152)) * (dig_H2) + 8192) >> 14));
-    
-    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) *
-			    (dig_H1)) >> 4));
-    
-    v_x1_u32r = (v_x1_u32r <0 )? 0: v_x1_u32r;
-    v_x1_u32r = (v_x1_u32r > 419430400)?419430400:v_x1_u32r;
-    float humidity = (v_x1_u32r>>12);
-    return humidity/1024.0;
 }
 
 // second delay user chooses how many seconds
@@ -346,6 +185,24 @@ void onTimeT0(){
 
 //open or close damper based on most recent temperature in the room
 void adjustDampers(){
+    //check room 1
+    if((temperatures[0] || temperatures[1]) > x){
+        
+    }else{
+        
+    }
+    //check room 2
+    if(temperatures[2] > x){
+        
+    }else{
+        
+    }
+    //check room 3
+    if(temperatures[3] > x){
+        
+    }else{
+        
+    }
     return;
 }
 
